6d2ddc5101ad15e064c304ada8793d341e21a625
iediong <ediongi6@hotmail.com>
2022-04-19 00:17:54 +0100

    refactor: refactor filter component to render a list of regions

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
       <div aria-label='banner' className='shadow-md w-full px-4 bg-white'>
         <div className='container mx-auto flex items-center justify-between py-10 '>
           <h1 className='font-extrabold dark:text-white'>Where in the world?</h1>
-          <div className='capitalize'>dark mode</div>
+          <button className='capitalize'>dark mode</button>
         </div>
       </div>
       <div aria-label='toolbar' className='px-4 '>
