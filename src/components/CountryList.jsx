b4fb5c807ac618dd3b10e5d1644e6137cd53030a
iediong <ediongi6@hotmail.com>
2022-04-19 02:07:52 +0100

    data fetch working

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Country from './Country';
 
-const CountryList = () => {
-  return <Country />;
-};
+class CountryList extends Component {
+  state = {
+    url: 'https://restcountries.com/v2/all',
+    countries: [],
+  };
+
+  async componentDidMount() {
+    let { url, countries: data } = this.state;
+    const response = await fetch(url);
+
+    data = await response.json();
+
+    this.setState({ countries: data });
+  }
+
+  render() {
+    console.log(this.state.countries[0]);
+    return <Country />;
+  }
+}
 
 export default CountryList;
