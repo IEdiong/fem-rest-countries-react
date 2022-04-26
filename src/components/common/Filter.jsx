7346076f2eabb177084977766475c61d07f9bb32
iediong <ediongi6@hotmail.com>
2022-04-26 14:34:34 +0100

    Use tailwindheadlessui filter component

--- src/components/common/Filter.jsx
+++ src/components/common/Filter.jsx
@@ -29,7 +29,7 @@ From: "transform opacity-100 scale-100"
 To: "transform opacity-0 scale-95" */}
 
       <div
-        className='origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none'
+        className='origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 z-10 focus:outline-none'
         role='menu'
         aria-orientation='vertical'
         aria-labelledby='filterByRegion-button'
