8a99d494bb4f1ba5f31eb68ec2e3eb210eebcae4
iediong <ediongi6@hotmail.com>
2022-05-26 06:54:02 +0100

    add filter collapse

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -3,33 +3,40 @@ import CountryCard from './CountryCard';
 
 const CountriesList = ({ name }) => {
   const [countries, setCountries] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const url = 'https://restcountries.com/v2/all';
     async function fetchData(url) {
-      const response = await fetch(url);
-      let data = await response.json();
-      let favArr = [];
-    
-      if (data) {
-        ['Germany', 'United States of America', 'Brazil', 'Iceland'].forEach((el) => {
-          let tempData = data.find((cty) => cty.name === el);
-          favArr.push(tempData);
-          data = data.filter((cty) => cty.name !== el);
-        });
-    
-        const fav = [...favArr, ...data];
-        setCountries(fav);
+      try {
+        const response = await fetch(url);
+        let data = await response.json();
+        let favArr = [];
+      
+        if (data) {
+          ['Germany', 'United States of America', 'Brazil', 'Iceland'].forEach((el) => {
+            let tempData = data.find((cty) => cty.name === el);
+            favArr.push(tempData);
+            data = data.filter((cty) => cty.name !== el);
+          });
+      
+          const fav = [...favArr, ...data];
+          setCountries(fav);
+          setIsLoading(false);
+        }
+      } catch (err) {
+        console.log(err.message);
       }
     }
     
     fetchData(url);
-    // console.log('fetch work');
+    console.log('fetch work');
   }, []);
 
   return (
     <main className='px-4'>
-      <div className='container mx-auto flex flex-wrap justify-between gap-16'>
+      <div className='container mx-auto flex flex-wrap gap-16'>
+        {isLoading && <div>Fetching data, Please wait...</div>}
         {countries.filter((cou) => cou.name.toLowerCase().includes(name.toLowerCase())).map((countryData) => (
           <CountryCard key={Number(countryData.numericCode)} country={countryData} />
         ))}
@@ -41,3 +48,5 @@ const CountriesList = ({ name }) => {
 export default CountriesList;
 
 // TODO: wrapper each CountryCard in an anchor tag, making it a list of links rather than a list of CountryCard
+
+// TODO: work on a better loading screen
--- src/components/common/Filter.jsx
+++ src/components/common/Filter.jsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import { useState } from 'react';
 
 const Filter = () => {
   const regions = ['Africa', 'America', 'Asia', 'Europe', 'Oceania'];
+  const [menuIsOpen, setMenuIsOpen] = useState(true);
+
+  const styles = () => {
+    if (menuIsOpen) {
+      return 'transition-opacity ease-out duration-200 origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 z-10 focus:outline-none transform opacity-100 scale-100'
+    } else {
+      return 'origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 z-0 focus:outline-none overflow-hidden h-0 mt-0 duration-150'
+    }
+  }
+
+  const handleToggle = () => {
+    setMenuIsOpen(!menuIsOpen);
+  }
+
+
   return (
     <div className='relative inline-block text-left w-56'>
       <div>
@@ -11,6 +26,7 @@ const Filter = () => {
           id='filterByRegion-button'
           aria-expanded='true'
           aria-haspopup='true'
+          onClick={handleToggle}
         >
           Filter by Region
           <svg className='-mr-1 ml-2 h-5 w-5' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20' fill='currentColor' aria-hidden='true'>
@@ -29,7 +45,7 @@ From: "transform opacity-100 scale-100"
 To: "transform opacity-0 scale-95" */}
 
       <div
-        className='origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 z-10 focus:outline-none'
+        className={styles()}
         role='menu'
         aria-orientation='vertical'
         aria-labelledby='filterByRegion-button'
@@ -38,7 +54,14 @@ To: "transform opacity-0 scale-95" */}
         <div className='py-1' role='none'>
           {/* <!-- Active: "bg-gray-100 text-gray-900", Not Active: "text-gray-700" --> */}
           {regions.map((region, id) => (
-            <a key={id} href='/' className='text-gray-700 block px-4 py-2 text-sm transition-all ease-in-out hover:bg-gray-100 hover:text-gray-900 hover:pl-6' role='menuitem' tabIndex='-1' id='menu-item-0'>
+            <a 
+            key={id}
+            href='/'
+            className='text-gray-700 block px-4 py-2 text-sm transition-all ease-in-out hover:bg-gray-100 hover:text-gray-900 hover:pl-6'
+            role='menuitem'
+            tabIndex='-1'
+            id='menu-item-0'
+            >
               {region}
             </a>
           ))}
